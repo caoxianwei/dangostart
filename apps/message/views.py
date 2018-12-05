2e954d2fe62a05568a41ea60ae5cd3c5d2619c16
sunday <1756862146@qq.com>
2018-12-05 22:12:02 +0800

    过滤查询数据

--- apps/message/views.py
+++ apps/message/views.py
@@ -4,7 +4,8 @@ from .models import UserMessage
 # Create your views here.
 
 def getfrom(request):
-    all_messages = UserMessage.objects.all()
+    # all_messages = UserMessage.objects.all()
+    all_messages = UserMessage.objects.filter(name='sunday_test', address='北京')
     for magssage in all_messages:
         print(magssage.name)
     return render(request, 'message_form.html')
